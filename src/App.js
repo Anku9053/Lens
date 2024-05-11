11d12073f8600c1f1c2c850ed92d86061b5bf4ac
Ankesh Kumar <ankesh9053@gmail.com>
2024-05-11 08:33:58 +0530

    only three things are  remaining

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Video from './Components/Video/Video';
+import About from './Components/About/About';
+import Services from './Components/Services/Services';
+import Why_Us from './Components/Why_Choose_Us/Why_Us';
+import Blogs from './Components/Blogs/Blogs';
+import Map from './Components/Map/Map';
+import Footer from './Components/Footer/Footer';
+import { Questions } from './Components/Questions/Questions';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-black' >
+      <Navbar/>
+      <Video/>
+      <About/>
+      <Services/>
+      <Why_Us/>
+      <Blogs/>
+      <Questions/>
+      <Map/>
+      <Footer/>
     </div>
   );
 }
